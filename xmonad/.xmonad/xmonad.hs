be5db6c0c5affe650758b2f833c9e569b2bb1a69
vinaran <vinaran@users.noreply.github.com>
2018-04-15 21:32:01 +0000

    xmonad: configure layout and borders
    This config follows the standard approach taken in the example
    dot files.
    
    SmartBorders removes borders when there is only one screen, there
    is only one window and it is floated and takes up the entire
    screen.
    
    NoBorders removes borders when a tile is full screen.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,5 +1,14 @@
 import XMonad
 import XMonad.Util.Run
+import XMonad.Layout.NoBorders
+import XMonad.Layout
+
+-- layout
+layout = tall ||| wide ||| full
+  where
+    tall = smartBorders $ Tall 1 0.03 0.5
+    wide = Mirror tall
+    full = noBorders Full
     
 main = do
     xmproc <- spawnPipe "sleep 0.1;polybar -r montecristo"  
@@ -7,5 +16,6 @@ main = do
         terminal = "urxvt",
         modMask = mod4Mask,
         normalBorderColor = "#705129",
-        focusedBorderColor = "#A17917"
+        focusedBorderColor = "#A17917",
+        layoutHook = layout
     }
