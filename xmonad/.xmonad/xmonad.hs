f0c3ffbd5eca8897f703ae7be1c28a098e9b9bd0
vinaran <vinaran@users.noreply.github.com>
2018-04-15 20:59:33 +0000

    xmonad: change focused border colour

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,9 +1,11 @@
 import XMonad
 import XMonad.Util.Run
-
+    
 main = do
     xmproc <- spawnPipe "sleep 0.1;polybar -r montecristo"  
     xmonad $ defaultConfig {
         terminal = "urxvt",
-        modMask = mod4Mask
+        modMask = mod4Mask,
+        normalBorderColor = "#705129",
+        focusedBorderColor = "#A17917"
     }
