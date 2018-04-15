a79dc1254fc86e629540c35f7026e1ea1c80ff78
vinaran <vinaran@users.noreply.github.com>
2018-04-15 20:47:56 +0000

    xmonad: remove unused dependencies

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,16 +1,5 @@
 import XMonad
-import XMonad.Actions.CycleWS
-import XMonad.Config.Desktop
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.ManageDocks
-import XMonad.Layout.DecorationMadness
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Renamed
-import XMonad.Layout.Spacing
-import XMonad.Util.EZConfig
 import XMonad.Util.Run
-import XMonad.Hooks.ManageHelpers 
 
 main = do
     xmproc <- spawnPipe "sleep 0.1;polybar -r montecristo"  
