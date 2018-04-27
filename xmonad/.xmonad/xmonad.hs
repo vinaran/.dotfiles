63ebf9b3881eca974cfc929407f796bc158a8495
vinaran <vinaran@users.noreply.github.com>
2018-04-27 21:39:45 +0000

    xmonad: prettify config
    After browsing other dotfiles on github, most of the dotfiles seem
    to have taken this approach to their formatting:
    - sections of code are clearly seperated
    - variables are prefixed with 'my'
    - everything is stored in a variable nothing is declared inline
    - commas at the beginning of the line

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -10,39 +10,66 @@ import XMonad.Util.EZConfig
 import XMonad.Layout.Fullscreen
 import XMonad.Hooks.ManageHelpers
 
+---------------------------------------------------------------
+-- colours
+---------------------------------------------------------------
+myNormalBorderColor  = "#705129"
+myFocusedBorderColor = "#A17917"
+
+---------------------------------------------------------------
+-- applications
+---------------------------------------------------------------
+myStatusBar    = "sleep 0.1;polybar -r montecristo"
+myBrowser      = "firefox"
+myBrowserClass = "Firefox"
+myEditor       = "emacs"
+myEditorClass  = "Emacs"
+myTerminal     = "urxvt"
+
+---------------------------------------------------------------
 -- layout
+---------------------------------------------------------------
 layout = tall ||| wide ||| full
   where
     tall = smartBorders $ Tall 1 0.03 0.5
     wide = Mirror tall
     full = noBorders Full
 
+---------------------------------------------------------------
 -- hooks
+---------------------------------------------------------------
 myManageHook :: ManageHook
 myManageHook = composeAll
-                [ isFullscreen          --> doFullFloat,
-                className =? "Emacs"    --> doShift "code",
-                className =? "Firefox"    --> doShift "web",
-                fullscreenManageHook,
-                manageDocks
-                ]
+                [ fullscreenManageHook
+                , manageDocks
+                , isFullscreen                --> doFullFloat
+                , className =? myEditorClass  --> doShift "code"
+                , className =? myBrowserClass --> doShift "web"]
 
+---------------------------------------------------------------
+-- workspaces
+---------------------------------------------------------------
+myWorkspaces = ["code", "web", "3", "4", "5", "6", "7"]
+
+---------------------------------------------------------------
+-- main
+---------------------------------------------------------------
 main = do
-    xmproc <- spawnPipe "sleep 0.1;polybar -r montecristo"
-    spawn "emacs"
-    spawn "firefox"
+    xmproc <- spawnPipe myStatusBar
+    spawn myEditor
+    spawn myBrowser
     
-    xmonad $ defaultConfig {
-        terminal = "urxvt",
-        modMask = mod4Mask,
-        normalBorderColor = "#705129",
-        focusedBorderColor = "#A17917",
-        layoutHook = desktopLayoutModifiers layout,
-        handleEventHook = handleEventHook desktopConfig <+> fullscreenEventHook,
-        logHook = ewmhDesktopsLogHook,
-        workspaces = ["code", "web", "3", "4", "5", "6", "7"],
-        startupHook = ewmhDesktopsStartup,
-        manageHook = myManageHook <+> manageHook defaultConfig
+    xmonad $ defaultConfig
+           {terminal           = myTerminal
+           ,modMask            = mod4Mask
+           ,normalBorderColor  = myNormalBorderColor
+           ,focusedBorderColor = myFocusedBorderColor
+           ,layoutHook         = desktopLayoutModifiers layout
+           ,handleEventHook    = handleEventHook desktopConfig <+> fullscreenEventHook
+           ,logHook            = ewmhDesktopsLogHook
+           ,workspaces         = myWorkspaces
+           ,startupHook        = ewmhDesktopsStartup
+           ,manageHook         = myManageHook <+> manageHook defaultConfig
     }  `additionalKeys`
       -- mod+b - will hide the bar
       [ ((mod4Mask, xK_b), sendMessage ToggleStruts) ]
